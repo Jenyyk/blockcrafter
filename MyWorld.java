ebf141d200edb9686ef9067a920b1d6dccc3e472
Jenyyk <jendotron2@gmail.com>
2025-01-07 11:09:50 +0100

    fix: hotbar is now drawn over blocks

--- MyWorld.java
+++ MyWorld.java
@@ -9,11 +9,13 @@ public class MyWorld extends World
         player = new Player(this, hotbar);
         addObject(player, 400, 400);
         addObject(hotbar, 400, 700);
-        
+
         addObject(new Block(1, 0, 65, this), 0, 0);
         addObject(new Block(1, 1, 65, this), 0, 0);
         addObject(new Block(1, 2, 65, this), 0, 0);
         addObject(new Block(1, -1, 64, this), 0, 0);
         addObject(new Block(1, 3, 64, this), 0, 0);
+
+        setPaintOrder(Player.class, Hotbar.class);
     }
 }
